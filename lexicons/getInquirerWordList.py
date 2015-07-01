5d21b247c72c85e430641816a056d9c9f310c5f0
justinek <justinek@stanford.edu>
2015-07-01 11:23:07 -0700

    script for generating inquirer wordlist

---
+++ lexicons/getInquirerWordList.py
@@ -0,0 +1,20 @@
+import sys, re, string
+
+f = open("../Materials/inquireraugmented.csv", "r")
+tag = sys.argv[1]
+
+linenum = 0
+for l in f:
+    l = l.strip()
+    if linenum == 0:
+        toks = l.split(",")
+        index = toks.index(tag)
+        linenum += 1
+    elif linenum == 1:
+        linenum += 1
+    elif linenum > 1:
+        toks = l.split(",")
+        word = toks[0].lower().split("#")[0]
+        if len(toks[index]) > 1:
+            print word
+    
