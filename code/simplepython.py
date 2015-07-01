e4f04180914682793844b216eae38eb58762519a
justinek <justinek@stanford.edu>
2015-07-01 16:00:55 -0700

    simple python

--- code/simplepython.py
+++ code/simplepython.py
@@ -1,4 +1,10 @@
-import pico
+# !/usr/
+import cgi, cgitb,requests, re
+
+cgitb.enable()
+form = cgi.FieldStorage()
+print form.getvalue("data")
+
 
 def returnColors(text):
     return ["blue", "green"]
