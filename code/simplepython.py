9ef7bf0c846cd05788ad14936a9e9ef5da7f36cb
justinek <justinek@stanford.edu>
2015-07-01 16:51:10 -0700

    cgi

--- code/simplepython.py
+++ code/simplepython.py
@@ -13,10 +13,54 @@
 import sys, string
 import json
 import cgi
+import pickle
 #from annotator import SentimentAnnotator
 #from colorPicker import colorPicker
 
 fs = cgi.FieldStorage()
+
+
+class SentimentAnnotator():
+	def __init__(self):
+		self.bingliu_pos = pickle.load(open("../pickledData/liu_pos.p", "r"))
+		self.bingliu_neg = pickle.load(open("../pickledData/liu_neg.p", "r"))
+		self.anew = pickle.load(open("../pickledData/anew.p", "r"))
+		self.warriner = pickle.load(open("../pickledData/warriner.p", "r"))
+
+	def annotateDiscreteLiu(self, word):
+		if word in self.bingliu_pos:
+			return 6
+		elif word in self.bingliu_neg:
+			return 4
+		else:
+			return 0
+
+	def annotateWarriner(self, word):
+		if word in self.warriner:
+			return self.warriner[word]
+		else:
+			return 0
+
+	def annotateAnew(self, word):
+		if word in self.anew:
+			return self.anew[word]
+		else:
+			return 0
+
+
+	# This is the function that should be called externally
+	def annotate(self, sentence, model="liu"):
+		words = sentence.split()
+		if model=="liu":
+			values = [self.annotateDiscreteLiu(word) for word in words]
+		elif model=="warriner":
+			values = [self.annotateWarriner(word) for word in words]
+		elif model=="anew":
+			values = [self.annotateAnew(word) for word in words]
+		else: # default
+			values = [self.annotateDiscreteLiu(word) for word in words]
+		return values
+
 
 def colorPicker(scores):
 	colors = []
