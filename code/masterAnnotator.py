934b7d4fbefa7493a3782e306e6ac8d7d80d593c
justinek <justinek@stanford.edu>
2015-07-01 17:34:38 -0700

    cgi

--- code/masterAnnotator.py
+++ code/masterAnnotator.py
@@ -22,10 +22,10 @@ fs = cgi.FieldStorage()
 
 class SentimentAnnotator():
 	def __init__(self):
-		self.bingliu_pos = pickle.load(open("../pickledData/liu_pos.p", "r"))
-		self.bingliu_neg = pickle.load(open("../pickledData/liu_neg.p", "r"))
-		self.anew = pickle.load(open("../pickledData/anew.p", "r"))
-		self.warriner = pickle.load(open("../pickledData/warriner.p", "r"))
+		self.bingliu_pos = pickle.load(open("pickledData/liu_pos.p", "r"))
+		self.bingliu_neg = pickle.load(open("pickledData/liu_neg.p", "r"))
+		self.anew = pickle.load(open("pickledData/anew.p", "r"))
+		self.warriner = pickle.load(open("pickledData/warriner.p", "r"))
 
 	def annotateDiscreteLiu(self, word):
 		if word in self.bingliu_pos:
