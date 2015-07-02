82c88066632fde43da41e9e1920a110fd2482716
desmond-ong <dco@stanford.edu>
2015-07-01 17:51:14 -0700

    fixed change

--- code/masterAnnotator.py
+++ code/masterAnnotator.py
@@ -24,7 +24,7 @@ class SentimentAnnotator():
 	def __init__(self):
 		self.bingliu_pos = pickle.load(open("../pickledData/liu_pos.p", "r"))
 		self.bingliu_neg = pickle.load(open("../pickledData/liu_neg.p", "r"))
-		self.anew = pickle.load(open("../pickledData/anew.p", "r"))
+		#self.anew = pickle.load(open("../pickledData/anew.p", "r"))
 		self.warriner = pickle.load(open("../pickledData/warriner.p", "r"))
 
 	def annotateDiscreteLiu(self, word):
